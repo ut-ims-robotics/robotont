5638d54aa3d0c4e65797fc9f9b800f36d40e9330
Veiko Vunder <veix123@gmail.com>
2018-09-27 15:56:04 +0300

    auto ip set for tontmasterX hosts

--- setup/setup_robotont.sh
+++ setup/setup_robotont.sh
@@ -4,7 +4,7 @@ echo 'This script configures ROS_PACKAGE_PATH, ROS_MASTER_URI, and ROS_IP'
 source ~/catkin_ws/devel/setup.bash
 
 # Set up robotont-2 as ROS MASTER
-export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_MASTER_URI=http://10.42.0.${HOSTNAME:10:1}:11311
 
 # Use ifconfig to get the wifi interface IP
 export ROS_IP=$(ifconfig wlp62s0 | grep -oP 'inet addr:\K\S+') # ethernet
