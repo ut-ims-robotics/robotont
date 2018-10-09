5de3ddb16467236be59e046af09105d99bdcf5d7
Madis <madis1995@gmail.com>
2018-10-09 20:32:20 +0300

    Added simulated odom

--- robotont_driver/src/odom_node.cpp
+++ robotont_driver/src/odom_node.cpp
@@ -132,7 +132,7 @@ int main(int argc, char** argv) {
 	    geometry_msgs::TransformStamped odom_trans;
 	    odom_trans.header.stamp = current_time;
 	    odom_trans.header.frame_id = "odom";
-	    odom_trans.child_frame_id = "base_base_link";
+	    odom_trans.child_frame_id = "base_link";
 	    
 	    odom_trans.transform.translation.x = x;
 	    odom_trans.transform.translation.y = y;
