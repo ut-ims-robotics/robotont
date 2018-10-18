902c077354b9c0b246193765d1d39c08edfbd69d
Madis <madis1995@gmail.com>
2018-10-18 11:31:09 +0300

    minor fixes to simulation, discovered that realtime factor is very small

--- robotont_driver/src/gazebo_odom.cpp
+++ robotont_driver/src/gazebo_odom.cpp
@@ -3,34 +3,37 @@
 #include <tf/transform_broadcaster.h>
 #include <string>
 
-class OdomTransformer {
- public:
-  OdomTransformer() {
+class OdomTransformer
+{
+public:
+  OdomTransformer()
+  {
     sub_ = n_.subscribe("odom", 100, &OdomTransformer::odom_callback, this);
   }
-  void odom_callback(nav_msgs::Odometry odom) {
+  void odom_callback(nav_msgs::Odometry odom)
+  {
     geometry_msgs::TransformStamped odom_trans;
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(odom.twist.twist.angular.z);
 
     odom_trans.header.stamp = ros::Time::now();
     odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
+    odom_trans.child_frame_id = "base_footprint";
 
     odom_trans.transform.translation.x = odom.twist.twist.linear.x;
     odom_trans.transform.translation.y = odom.twist.twist.linear.y;
     odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation = odom.pose.pose.orientation;
+    odom_trans.transform.rotation = odom_quat;
 
     odom_broadcaster.sendTransform(odom_trans);
   }
 
- private:
+private:
   ros::NodeHandle n_;
   tf::TransformBroadcaster odom_broadcaster;
   ros::Subscriber sub_;
 };
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gazebo_odom_node");
 
