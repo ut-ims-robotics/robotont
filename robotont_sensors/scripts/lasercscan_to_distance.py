11efa5f4a488101094452b259b065aa32c623b64
Madis Kaspar Nigol <madis1995@gmail.com>
2018-11-11 16:22:25 +0200

    created a custom message for the laserscan splitter and modified the script accordingly

--- robotont_sensors/scripts/lasercscan_to_distance.py
+++ robotont_sensors/scripts/lasercscan_to_distance.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 import rospy
 import time
+from numpy import mean
 from std_msgs.msg import Float64
 from sensor_msgs.msg import LaserScan
+from robotont_sensors.msg import LaserScanSplit
 
-pub1 = rospy.Publisher('/scan/left', Float64, queue_size=2)
-pub2 = rospy.Publisher('/scan/center', Float64, queue_size=2)
-pub3 = rospy.Publisher('/scan/right', Float64, queue_size=2)
+pub = rospy.Publisher('/laserscan_split', Float64, queue_size=2)
 
 def scan_callback(data):
+    ls_split = LaserScanSplit()
+
+    ls_split.stamp = rospy.Time()
+
     distances = data.ranges
     array_len = len(distances)
 
     left_array = distances[0:array_len/3]
-    left_dist = min(left_array)
+    ls_split.leftMin = min(left_array)
+    ls_split.leftMean = mean(left_array)
 
     center_array = distances[array_len/3:2*array_len/3]
-    center_dist = min(center_array
+    ls_split.centerMin = min(center_array)
+    ls_split.centerMean = mean(center_array)
 
     right_array = distances[2*array_len/3:]
-    right_dist = min(right_array)
+    ls_split.rightMin = min(right_array)
+    ls_split.rightMean = mean(right_array)
 
-    pub1.publish(left_dist)
-    pub2.publish(center_dist)
-    pub3.publish(rights_dist)
+    pub.publish(left_dist)
 
 def convert():
     # Starts a new node
