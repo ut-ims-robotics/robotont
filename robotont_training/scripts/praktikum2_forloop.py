5529baff332379afd389ae6da02d25c654b57d55
Madis Kaspar Nigol <madis1995@gmail.com>
2018-12-13 12:19:32 +0200

    added robotont_training package for the course starting scripts

---
+++ robotont_training/scripts/praktikum2_forloop.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+import time
+
+def move():
+    # Starts a new node
+    rospy.init_node('robotont_velocity_publisher', anonymous=True)
+    velocity_publisher = rospy.Publisher('/robotont/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    vel_msg.linear.x = 0
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+    
+    while not rospy.is_shutdown():
+        
+        try:
+            ########################
+            # YOUR CODE HERE START #
+            ########################
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.angular.z = 0
+            velocity_publisher.publish(vel_msg)
+            time.sleep(0.1)
+            ######################
+            # YOUR CODE HERE END #
+            ######################
+
+            
+        except KeyboardInterrupt:
+            #After the loop, stops the robot
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = 0
+            #Force the robot to stop
+            velocity_publisher.publish(vel_msg)
+            time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        move()
+    except rospy.ROSInterruptException: pass
